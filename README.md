329ed089f792a4dc3713b081567faa103e389418
kneeki <david.tacker@gmail.com>
2017-01-29 15:16:16 -1000

    Initial upload.

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# json-resume-Dave
+# jsonresume-theme-Dave
+A compact theme for JSON Resume, designed for printing.
+
+Tries to fit as much information as possible onto a single page without making sections look cluttered.
+
+## Table of Contents
+
+* [Preview](#preview)
+* [Example](#example)
+* [Running](#running)
+* [Options](#options)
+* [License](#license)
+
+## Preview
+![Preview](preview.png)
+
+## Example
+http://themes.jsonresume.org/theme/dave
+
+## Running
+
+```sh
+# If you need to install Node.js:
+#    sudo apt-get install npm
+sudo npm install -g resume-cli
+git clone https://github.com/kneeki/jsonresume-theme-Dave.git
+cd jsonresume-theme-dave
+sudo npm install
+resume serve
+```
+You can print directly from the served html.
+
+## Options
+
+You may toggle `SORT__KEYWORDS` to alphabetically sort the keywords (useful for long arrays). See `index.js`. The interest section is no longer denoted with a `.sectionName`. Instead, each `interest[i].name` has the `.sectionName` class.
+
+## License
+The MIT License (MIT)
+
+Copyright (c) 2015 Ainsley Chong
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ index.js
@@ -0,0 +1,97 @@
+/*jslint node: true, white: true, nomen: true, plusplus: true, sloppy: true*/
+
+var fs = require("fs"),
+    Handlebars = require("handlebars");
+
+/**
+ * Render the Handlebars template
+ * @param   {object} resume
+ * @returns {object}
+ */
+function render(resume) {
+
+    var css = fs.readFileSync(__dirname + "/style.css", "utf-8"),
+        template = fs.readFileSync(__dirname + "/resume.hbs", "utf-8"),
+        SORT_KEYWORDS = true,
+        CLEAN_DATES = true;
+
+    /**
+     * Validates an array
+     * @param   {Array}   arr
+     * @returns {boolean}
+     */
+    function validateArray(arr) {
+        return arr !== undefined && arr !== null && arr instanceof Array && arr.length > 0;
+    }
+
+    /**
+     * Format a date string to return just the year
+     * @param   {string} date
+     * @returns {string}
+     */
+    function formatDate(date, format) {
+        if (date.length === 0) {
+            return "";
+        }
+
+        var retVal = "",
+            d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+
+        if (month.length < 2) {
+            month = '0' + month;
+        }
+
+        if (day.length < 2) {
+            day = '0' + day;
+        }
+
+        if (format === 0) {
+            retVal = year;
+        } else if (format === 1) {
+            retVal = month + "/" + year;
+        } else {
+            return day + "/" + month + "/" + year;
+        }
+
+        return retVal;
+    }
+
+
+    if (CLEAN_DATES) {
+        // Modify the dates for education
+        if (validateArray(resume.education)) {
+            resume.education.forEach(function (block) {
+                block.startDate = formatDate(block.startDate, 0);
+                block.endDate = formatDate(block.endDate, 0);
+            });
+        }
+
+        // Modify the dates for work
+        if (validateArray(resume.work)) {
+            resume.work.forEach(function (block) {
+                block.startDate = formatDate(block.startDate, 1);
+                block.endDate = formatDate(block.endDate, 1);
+            });
+        }
+    }
+
+
+    // Sort the keywords for varying areas.
+    if (SORT_KEYWORDS && validateArray(resume.skills)) {
+        resume.skills.forEach(function (skill) {
+            skill.keywords.sort();
+        });
+    }
+
+    return Handlebars.compile(template)({
+        css: css,
+        resume: resume
+    });
+}
+
+module.exports = {
+    render: render
+};
