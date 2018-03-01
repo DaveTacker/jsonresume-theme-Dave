0f6987ba4294dbfaefb2a5eda4200b88df1c54c4
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-03-01 15:02:23 -0500

    Update to my theme dave-alt

--- index.js
+++ index.js
@@ -2,6 +2,30 @@
 
 var fs = require("fs"),
     Handlebars = require("handlebars");
+    moment = require("moment");
+
+Handlebars.registerHelper("stripHttp", function(website) {
+  return website.replace(/https?:\/\/(www\.)?/gi, "");
+});
+
+function GetSortOrder(prop, reverse = true){
+   reverse = reverse ? -1 : 1;
+   return function(a,b){
+      if( a[prop] > b[prop]){
+          return 1 * reverse;
+      }else if( a[prop] < b[prop] ){
+          return -1 * reverse;
+      }
+      return 0;
+   }
+}
+
+function getFormattedDate(date, date_format) {
+    var DATE_FORMAT_INPUT = 'YYYY-MM-DD'; // resume.json standard date format
+    date_format = date_format || config.date_format; // output format
+
+    return moment(date, DATE_FORMAT_INPUT).format(date_format);
+}
 
 /**
  * Render the Handlebars template
@@ -12,7 +36,8 @@ function render(resume) {
 
     var css = fs.readFileSync(__dirname + "/style.css", "utf-8"),
         template = fs.readFileSync(__dirname + "/resume.hbs", "utf-8"),
-        SORT_KEYWORDS = true,
+        SORT_KEYWORDS = false,
+        SORT_WORK_BY_ENDDATE = true,
         CLEAN_DATES = true;
 
     /**
@@ -37,94 +62,70 @@ function render(resume) {
         return retVal;
     }
 
-    /**
-     * Format a date string to return just the year
-     * @param   {string} date
-     * @returns {string}
-     */
-    function formatDate(date, format) {
-        if (date.length === 0) {
-            return "";
-        }
-
-        var retVal = "",
-            d = new Date(date),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear();
-
-        if (month.length < 2) {
-            month = '0' + month;
-        }
-
-        if (day.length < 2) {
-            day = '0' + day;
-        }
-
-        if (format === 0) {
-            retVal = year;
-        } else if (format === 1) {
-            retVal = month + "/" + year;
-        } else {
-            return day + "/" + month + "/" + year;
-        }
-
-        return retVal;
+    // Sort work by endDate
+    if (SORT_WORK_BY_ENDDATE) {
+        resume.work.sort(GetSortOrder("endDate"));
     }
-
 
     if (CLEAN_DATES) {
         // Modify the dates for education
         if (validateArray(resume.education)) {
             resume.education.forEach(function (block) {
-                block.startDate = formatDate(block.startDate, 0);
-                block.endDate = formatDate(block.endDate, 0);
+                block.startDate = getFormattedDate(block.startDate, "YYYY");
+                block.endDate = getFormattedDate(block.endDate, "YYYY");
             });
         }
 
-        // Modify the dates for volunteering
-        if (validateArray(resume.experience)) {
-            resume.experience.forEach(function (block) {
-                block.startDate = formatDate(block.startDate, 0);
-                block.endDate = formatDate(block.endDate, 0);
+        // Modify the dates for military
+        if (validateArray(resume.military)) {
+            resume.military.forEach(function (block) {
+                block.startDate = getFormattedDate(block.startDate, "YYYY");
+                block.endDate = getFormattedDate(block.endDate, "YYYY");
             });
         }
 
         // Modify the dates for volunteering
         if (validateArray(resume.volunteer)) {
             resume.volunteer.forEach(function (block) {
-                block.startDate = formatDate(block.startDate, 0);
-                block.endDate = formatDate(block.endDate, 0);
+                block.startDate = getFormattedDate(block.startDate, "YYYY");
+                block.endDate = getFormattedDate(block.endDate, "YYYY");
             });
         }
 
         // Modify the dates for work
         if (validateArray(resume.work)) {
             resume.work.forEach(function (block) {
-                block.startDate = formatDate(block.startDate, 1);
-                block.endDate = formatDate(block.endDate, 1);
+                block.startDate = getFormattedDate(block.startDate, "MMM YYYY");
+                block.endDate = getFormattedDate(block.endDate, "MMM YYYY");
             });
         }
 
         // Modify the dates for publications
         if (validateArray(resume.publications)) {
             resume.publications.forEach(function (block) {
-                block.releaseDate = formatDate(block.releaseDate, 1);
+                block.releaseDate = getFormattedDate(block.releaseDate, "YYYY");
+            });
+        }
+      
+        // Modify the dates for presentations
+        if (validateArray(resume.presentations)) {
+            resume.presentations.forEach(function (block) {
+                block.date = getFormattedDate(block.date, "YYYY-MM-DD");
             });
         }
 
-        // Modify the dates for publications
+        // Modify the dates for awards
         if (validateArray(resume.awards)) {
             resume.awards.forEach(function (block) {
-                block.date = formatDate(block.date, 1);
+                block.date = getFormattedDate(block.date, "MMM YYYY");
             });
         }
 
         // Modify the dates for certifications
         if (validateArray(resume.certifications)) {
             resume.certifications.forEach(function (block) {
-                block.startDate = formatDate(block.startDate, 1);
-                block.endDate = formatDate(block.endDate, 1);
+                block.startDate = getFormattedDate(block.startDate, "MMM YYYY");
+                block.endDate = getFormattedDate(block.endDate, "MMM YYYY");
             });
         }
     }
